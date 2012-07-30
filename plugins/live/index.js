7f1c72521a4b01a03ef4b335d2c214bca4faeacf
Lars Jung <lrsjng@gmail.com>
2012-07-31 01:07:44 +0200

    Adds live reload plugin:

---
+++ plugins/live/index.js
@@ -0,0 +1,40 @@
+/*jshint node: true */
+'use strict';
+
+var fs = require('fs'),
+	_ = require('underscore'),
+
+	live_content = fs.readFileSync(__dirname + '/live-fquery.js', 'utf-8'),
+
+	template = '<script>' + live_content + '</script>';
+
+
+module.exports = function (fQuery) {
+
+	return {
+
+		live: function (view) {
+
+			var fquery = this;
+
+			return this.edit(function (blob) {
+
+				try {
+					var content = blob.content;
+
+					content = content.replace('</head>', template.replace('LIVE_HINT', '"countUp"') + '</head>');
+
+					blob.content = content;
+				} catch (err) {
+					fQuery.error({
+						method: 'live',
+						message: err.toString(),
+						fquery: fquery,
+						blob: blob,
+						data: err
+					});
+				}
+			});
+		}
+	};
+};
