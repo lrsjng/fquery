43bddebdb45b018da1ec2c7c28435a3459c3d914
Lars Jung <lrsjng@gmail.com>
2013-08-15 20:50:08 +0200

    Updates docco but still not working until next npm release of docco.

--- plugins/docco/index.js
+++ plugins/docco/index.js
@@ -1,19 +1,45 @@
 /*jshint node: true */
 'use strict';
 
-var path = require('path'),
-	_ = require('underscore');
 
 
 module.exports = function (fQuery) {
 
+
+	var _ = require('underscore'),
+
+		// defaults = {
+		// 	layout: 'parallel',
+		// 	output: 'docs',
+		// 	template: null,
+		// 	css: null,
+		// 	extension: null
+		// }
+
+		defaults = {
+			callback: null
+		};
+
+
 	return {
 
-		docco: function (dest, callback) {
+		docco: function (options) {
+
+			var fquery = this,
+				settings = _.extend({}, defaults, options),
+				docco = require('docco');
 
 			fQuery.info({
 				method: 'docco',
-				message: 'not available yet',
+				message: 'generating docs',
+				fquery: this
+			});
+
+			settings.sources = _.pluck(this, 'source');
+			// docco.document(settings, settings.callback);
+			fQuery.error({
+				method: 'docco',
+				message: 'still broken until next npm release of docco',
 				fquery: this
 			});
 
