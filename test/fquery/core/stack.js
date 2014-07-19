d76a5d9f860bc6e8d926b656b3a9715b8ca4665b
Lars Jung <lrsjng@gmail.com>
2014-07-19 03:24:58 +0200

    Add tests.

---
+++ test/fquery/core/stack.js
@@ -0,0 +1,441 @@
+/*jshint node: true */
+/*global describe, before, beforeEach, it */
+
+
+var assert = require('assert'),
+	_ = require('underscore'),
+
+	slice = function (obj) {
+
+		return Array.prototype.slice.call(obj);
+	},
+
+	fQuery = require('../../../lib/fquery/fQuery');
+
+
+describe('fQuery.fn.push()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.push));
+	});
+
+	it('expectes 1 parameter', function () {
+
+		assert.strictEqual(fQuery.fn.push.length, 1);
+	});
+
+	it('works with no parameter', function () {
+
+		fQuery().push();
+		fQuery().push(undefined);
+		fQuery().push(null);
+	});
+
+	it('returns this, is chainable', function () {
+
+		var x = fQuery();
+		var y = x.push();
+		var z = y.push();
+		assert.strictEqual(x, y);
+		assert.strictEqual(y, z);
+	});
+
+	it('initial stack okay for empty selection', function () {
+
+		var x = fQuery();
+		assert.deepEqual(x._stack, [[]]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for empty push', function () {
+
+		var x = fQuery().push();
+		assert.deepEqual(x._stack, [[], []]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for multi empty push', function () {
+
+		var x = fQuery().push().push();
+		assert.deepEqual(x._stack, [[], [], []]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for non blob value push', function () {
+
+		var v = {};
+		var x = fQuery().push(v);
+		assert.deepEqual(x._stack, [[], []]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for non blobs array push', function () {
+
+		var v = [{}, 1, true, null, undefined, 'text'];
+		var x = fQuery().push(v);
+		assert.deepEqual(x._stack, [[], []]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for blob value push', function () {
+
+		var b = fQuery.Blob.select('test/assets/a');
+		var x = fQuery().push(b);
+		assert.deepEqual(x._stack, [[b], []]);
+		assert.deepEqual(slice(x), [b]);
+		assert.strictEqual(x.length, 1);
+	});
+
+	it('stack okay for blob array push', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]);
+		assert.deepEqual(x._stack, [[b1, b2, b3], []]);
+		assert.deepEqual(slice(x), [b1, b2, b3]);
+		assert.strictEqual(x.length, 3);
+	});
+
+	it('stack okay for mixed array push', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, 1, b2, true, b3, null]);
+		assert.deepEqual(x._stack, [[b1, b2, b3], []]);
+		assert.deepEqual(slice(x), [b1, b2, b3]);
+		assert.strictEqual(x.length, 3);
+	});
+
+	it('stack okay for multi blob array push', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]).push([b1]).push(b3);
+		assert.deepEqual(x._stack, [[b3], [b1], [b1, b2, b3], []]);
+		assert.deepEqual(slice(x), [b3]);
+		assert.strictEqual(x.length, 1);
+	});
+});
+
+
+
+describe('fQuery.fn.pop()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.pop));
+	});
+
+	it('expectes 0 parameters', function () {
+
+		assert.strictEqual(fQuery.fn.pop.length, 0);
+	});
+
+	it('works with parameter', function () {
+
+		fQuery().pop();
+		fQuery().pop(undefined);
+		fQuery().pop(null);
+	});
+
+	it('returns this, is chainable', function () {
+
+		var x = fQuery();
+		var y = x.pop();
+		var z = y.pop();
+		assert.strictEqual(x, y);
+		assert.strictEqual(y, z);
+	});
+
+	it('stack okay for pop', function () {
+
+		var x = fQuery().pop();
+		assert.deepEqual(x._stack, [[]]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for multi pop 1', function () {
+
+		var x = fQuery().pop().pop();
+		assert.deepEqual(x._stack, [[]]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for multi pop 2', function () {
+
+		var x = fQuery().pop().pop().pop();
+		assert.deepEqual(x._stack, [[]]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+
+	it('stack okay for multi pop 3', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]).push([b3, b1]).push([b1]);
+
+		x.pop();
+		assert.deepEqual(x._stack, [[b3, b1], [b1, b2, b3], []]);
+		assert.deepEqual(slice(x), [b3, b1]);
+		assert.strictEqual(x.length, 2);
+
+		x.pop();
+		assert.deepEqual(x._stack, [[b1, b2, b3], []]);
+		assert.deepEqual(slice(x), [b1, b2, b3]);
+		assert.strictEqual(x.length, 3);
+
+		x.pop();
+		assert.deepEqual(x._stack, [[]]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+
+		x.pop();
+		assert.deepEqual(x._stack, [[]]);
+		assert.deepEqual(slice(x), []);
+		assert.strictEqual(x.length, 0);
+	});
+});
+
+
+describe('fQuery.fn.get()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.get));
+	});
+
+	it('expectes 1 parameter', function () {
+
+		assert.strictEqual(fQuery.fn.get.length, 1);
+	});
+
+	it('returns blob array if no parameter 1', function () {
+
+		var x = fQuery();
+		assert.deepEqual(x.get(), []);
+	});
+
+	it('returns blob array if no parameter 2', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]);
+		assert.deepEqual(x.get(), [b1, b2, b3]);
+	});
+
+	it('returns correct value if int parameter', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]);
+
+		assert.strictEqual(x.get(-4), undefined);
+		assert.strictEqual(x.get(-3), b1);
+		assert.strictEqual(x.get(-2), b2);
+		assert.strictEqual(x.get(-1), b3);
+		assert.strictEqual(x.get(0), b1);
+		assert.strictEqual(x.get(1), b2);
+		assert.strictEqual(x.get(2), b3);
+		assert.strictEqual(x.get(3), undefined);
+	});
+
+	it('returns undefined if non int numeric parameter', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]);
+
+		assert.strictEqual(x.get(1.5), undefined);
+	});
+});
+
+
+describe('fQuery.fn.each()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.each));
+	});
+
+	it('expectes 1 parameter', function () {
+
+		assert.strictEqual(fQuery.fn.each.length, 1);
+	});
+
+	it('works with no parameter', function () {
+
+		fQuery().each();
+		fQuery().each(undefined);
+		fQuery().each(null);
+	});
+
+	it('returns this, is chainable', function () {
+
+		var x = fQuery();
+		var y = x.each();
+		var z = y.each();
+		assert.strictEqual(x, y);
+		assert.strictEqual(y, z);
+	});
+
+	it('iterates correctly 1', function () {
+
+		var x = fQuery();
+		var list = [];
+
+		x.each(function (blob, idx) {
+
+			list.push([this, blob, idx]);
+		});
+
+		assert.deepEqual(list, []);
+	});
+
+	it('iterates correctly 2', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]);
+		var list = [];
+
+		x.each(function (blob, idx) {
+
+			list.push([this, blob, idx]);
+		});
+
+		var expected = [
+			[x, b1, 0],
+			[x, b2, 1],
+			[x, b3, 2]
+		];
+
+		assert.deepEqual(list, expected);
+	});
+
+	it('iterates correctly 3', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]).push([b2]);
+		var list = [];
+
+		x.each(function (blob, idx) {
+
+			list.push([this, blob, idx]);
+		});
+
+		var expected = [
+			[x, b2, 0]
+		];
+
+		assert.deepEqual(list, expected);
+	});
+});
+
+
+describe('fQuery.fn.map()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.map));
+	});
+
+	it('expectes 1 parameter', function () {
+
+		assert.strictEqual(fQuery.fn.map.length, 1);
+	});
+
+	it('works with no parameter', function () {
+
+		fQuery().map();
+		fQuery().map(undefined);
+		fQuery().map(null);
+	});
+
+	it('returns array', function () {
+
+		var x = fQuery();
+		assert.ok(_.isArray(x.map()));
+		assert.ok(_.isArray(x.map(undefined)));
+		assert.ok(_.isArray(x.map(null)));
+	});
+
+	it('iterates correctly 1', function () {
+
+		var x = fQuery();
+		var list = [];
+
+		var result = x.map(function (blob, idx) {
+
+			list.push([this, blob, idx]);
+			return 'v' + idx;
+		});
+
+		assert.deepEqual(list, []);
+		assert.deepEqual(result, []);
+	});
+
+	it('iterates correctly 2', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]);
+		var list = [];
+
+		var result = x.map(function (blob, idx) {
+
+			list.push([this, blob, idx]);
+			return 'v' + idx;
+		});
+
+		var expected = [
+			[x, b1, 0],
+			[x, b2, 1],
+			[x, b3, 2]
+		];
+
+		assert.deepEqual(list, expected);
+		assert.deepEqual(result, ['v0', 'v1', 'v2']);
+	});
+
+	it('iterates correctly 3', function () {
+
+		var b1 = fQuery.Blob.select('test/assets/a');
+		var b2 = fQuery.Blob.select('test/assets/b');
+		var b3 = fQuery.Blob.select('test/assets/c');
+		var x = fQuery().push([b1, b2, b3]).push([b2]);
+		var list = [];
+
+		var result = x.map(function (blob, idx) {
+
+			list.push([this, blob, idx]);
+			return 'v' + idx;
+		});
+
+		var expected = [
+			[x, b2, 0]
+		];
+
+		assert.deepEqual(list, expected);
+		assert.deepEqual(result, ['v0']);
+	});
+});
---
+++ test/fquery/core/then.js
@@ -0,0 +1,137 @@
+/*jshint node: true */
+/*global describe, before, beforeEach, it */
+
+
+var assert = require('assert'),
+	_ = require('underscore'),
+
+	fQuery = require('../../../lib/fquery/fQuery');
+
+
+describe('fQuery.fn.then()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.then));
+	});
+
+	it('expectes 1 parameter', function () {
+
+		assert.strictEqual(fQuery.fn.then.length, 1);
+	});
+
+	it('works with no parameter', function () {
+
+		fQuery().then();
+	});
+
+	it('returns this, is chainable', function () {
+
+		var x = fQuery();
+		var y = x.then();
+		var z = y.then();
+		assert.strictEqual(x, y);
+		assert.strictEqual(y, z);
+	});
+
+	it('then calls are in order', function () {
+
+		var list = [0];
+
+		fQuery()
+			.then(function () {
+
+				list.push(1);
+				assert.deepEqual(list, [0, 1]);
+			})
+			.then(function () {
+
+				list.push(2);
+				assert.deepEqual(list, [0, 1, 2]);
+			})
+			.then(function () {
+
+				list.push(3);
+				assert.deepEqual(list, [0, 1, 2, 3]);
+			});
+
+		setTimeout(function () {
+
+			assert.deepEqual(list, [0, 1, 2, 3]);
+		}, 0);
+
+		assert.deepEqual(list, [0]);
+	});
+});
+
+
+describe('fQuery.fn.isPending()', function () {
+
+	it('is function', function () {
+
+		assert.ok(_.isFunction(fQuery.fn.isPending));
+	});
+
+	it('expectes 1 parameter', function () {
+
+		assert.strictEqual(fQuery.fn.isPending.length, 1);
+	});
+
+	it('false for new object', function () {
+
+		var x = fQuery();
+		assert.strictEqual(x.isPending(), false);
+
+		x = fQuery('test/assets/*');
+		assert.strictEqual(x.isPending(), false);
+	});
+
+	it('true after call for then', function () {
+
+		var x = fQuery().then();
+		assert.strictEqual(x.isPending(), true);
+	});
+
+	it('true inside then', function (done) {
+
+		var x = fQuery().then(function () {
+
+				assert.strictEqual(x.isPending(), true);
+				done();
+			});
+	});
+
+	it('false after then is done', function (done) {
+
+		var x = fQuery().then(function () {
+
+				setTimeout(afterThen, 0);
+			}),
+			afterThen = function () {
+
+				assert.strictEqual(x.isPending(), false);
+				done();
+			};
+	});
+
+	it('correct in all phases', function (done) {
+
+		var x = fQuery(),
+			afterThen = function () {
+
+				assert.strictEqual(x.isPending(), false);
+				done();
+			};
+
+		assert.strictEqual(x.isPending(), false);
+
+		x.then(function () {
+
+			assert.strictEqual(x.isPending(), true);
+			setTimeout(afterThen, 0);
+			assert.strictEqual(x.isPending(), true);
+		});
+
+		assert.strictEqual(x.isPending(), true);
+	});
+});
